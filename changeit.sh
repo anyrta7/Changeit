cfe3bfb848e2f5024d6050982931135c01a29ab6
anyrta7 <anyrta7@gmail.com>
2024-11-03 18:23:07 +0700

    [Add] added support for new options: --add-first, --add-end, --select, --replace, --output, --backup, --quiet, and --verbose.

--- changeit.sh
+++ changeit.sh
@@ -8,3 +8,45 @@ quiet_mode=0
 verbose_mode=0
 declare -a select_texts
 declare -a replace_texts
+
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+    -af | --add-first)
+        add_first="$2"
+        shift 2
+        ;;
+    -ae | --add-end)
+        add_end="$2"
+        shift 2
+        ;;
+    -s | --select)
+        select_texts+=("$2")
+        shift 2
+        ;;
+    -r | --replace)
+        replace_texts+=("$2")
+        shift 2
+        ;;
+    -o | --output)
+        output_file="$2"
+        shift 2
+        ;;
+    -b | --backup)
+        backup_mode=1
+        shift
+        ;;
+    -q | --quiet)
+        quiet_mode=1
+        shift
+        ;;
+    -v | --verbose)
+        verbose_mode=1
+        shift
+        ;;
+    -h | --help) show_help ;;
+    *)
+        echo "Unknown option: $1"
+        show_help
+        ;;
+    esac
+done
